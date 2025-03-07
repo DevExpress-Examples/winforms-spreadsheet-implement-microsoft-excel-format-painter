38fb721308b25acc7d8ac62e4a3a3c8fa01b38b4
DevExpressExampleBot <fakeEmail>
2025-03-07 13:03:41 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsApplication1/Form1.vb
+++ VB/WindowsFormsApplication1/Form1.vb
@@ -1,18 +1,14 @@
-﻿Imports DevExpress.Spreadsheet
-Imports System
-Imports System.Linq
 Imports System.Windows.Forms
 
 Namespace WindowsFormsApplication1
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-			Dim formatPainterProvider As New FormatPainterProvider()
-			formatPainterProvider.RegisterFormatPainter(spreadsheetControl1, biFormatPainter)
-		End Sub
-	End Class
+    Public Partial Class Form1
+        Inherits Form
 
-
+        Public Sub New()
+            InitializeComponent()
+            Dim formatPainterProvider As FormatPainterProvider = New FormatPainterProvider()
+            formatPainterProvider.RegisterFormatPainter(spreadsheetControl1, biFormatPainter)
+        End Sub
+    End Class
 End Namespace
